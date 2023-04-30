7e58488fd4c7655da510772d39da9d9f39d78c41
Lee HyunMin <hmlee4135@naver.com>
2023-04-30 16:52:52 +0900

    Add PlayerSetting
    * Player Setting
     + Add Player Class
     + Add Player HP UI
     + Add Player Damage,Heal Effect
     + Add Danger HP Warning

---
+++ Like_BroPotato/Assets/4. HM/2. Scripts/Player/PlayerHP_UI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _4._HM._2._Scripts.Player
+{
+    public class PlayerHP_UI : MonoBehaviour
+    {
+        [SerializeField] private Player_Setting playerHp;
+        [SerializeField] private Slider hpSlider;
+        [SerializeField] private GameObject slider_FillArea;
+        [SerializeField] private Text hpCount;
+
+        private void Awake()
+        {
+            #region CheckNull SerializedField
+
+            Debug.Assert(playerHp, "Not < Player > Setting");
+            Debug.Assert(hpSlider, "Not < HPSlider > Setting");
+            Debug.Assert(slider_FillArea, "Not < Fill_Area > Setting");
+            Debug.Assert(hpCount, "Not < Hp_Count TXT > Setting");
+
+            #endregion
+
+            // 이벤트 구독 
+            playerHp.OnDamaged += ChangeHpSlider;
+            playerHp.OnHealed += ChangeHpSlider;
+            playerHp.OnDead += PlayerDied;
+
+            ChangeHpSlider();
+        }
+
+        // 데미지 또는 힐했을 때 HP 슬라이더 변경해야됨
+        private void ChangeHpSlider()
+        {
+            hpSlider.value = (float)playerHp.CurrentHp / playerHp.MaxHp;
+            hpCount.text = playerHp.CurrentHp.ToString() + " / " + playerHp.MaxHp.ToString();
+        }
+
+        // 플레이어가 죽었을 경우 이벤트 구독 해제를 통한 시스템 자원 관리
+        // FillArea를 비활성화 함으로써 HP바 수정
+        private void PlayerDied()
+        {
+            slider_FillArea.SetActive(false);
+            ChangeHpSlider();
+
+            playerHp.OnDamaged -= ChangeHpSlider;
+            playerHp.OnHealed -= ChangeHpSlider;
+            playerHp.OnDead -= PlayerDied;
+        }
+    }
+}
--- Like_BroPotato/Assets/4. HM/2. Scripts/Player/Player_Setting.cs
+++ Like_BroPotato/Assets/4. HM/2. Scripts/Player/Player_Setting.cs
@@ -1,26 +1,43 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _4._HM._2._Scripts.Player
 {
     public class Player_Setting : MonoBehaviour
     {
-        [SerializeField] protected int maxHp = 100;
-
+        [Header("## Player Status ##"), SerializeField] 
+        protected int maxHp = 100;
         private int currentHp;
-        private bool isDead = false;
+        private int sum_Damage = 0; // 내가 받은 모든 데미지
+        private int sum_Healing = 0; // 내가 총 힐한 양
+        private int sum_Kill = 0; // 내가 총 죽인 적 수
+
+        [Range(0f,1f)]
+        public float critical_HP_Ratio;
+        
+        [Space(5f),Header("## Invincible Time ##"), SerializeField] 
+        protected float invincibilityTime = 2f; // 무적 시간
+        
+        private bool isDead;
+        private bool isHitting;
 
         public int CurrentHp
         {
             get => currentHp;
             set
             {
+                currentHp = value;
+
                 if (currentHp <= 0)
                 {
                     currentHp = 0;
 
                     if (!isDead)
                         isDead = true;
+
+                    OnDead?.Invoke();
                 }
                 else if (currentHp > maxHp)
                 {
@@ -42,32 +59,66 @@ namespace _4._HM._2._Scripts.Player
             }
         }
 
-        public event Action<int> OnDamaged;
+        #region ## Events
+
+        // 데미지를 입었을 때 발생하는 이벤트
+        public event Action OnDamaged;
+        // 회복했을 떄 발생하는 이벤트
+        public event Action OnHealed;
+        // 죽었을 때 발생하는 이벤트
+        public event Action OnDead;
+
+        #endregion
         
-        protected virtual void OnEnable()
+        protected void OnEnable()
         {
             CurrentHp = MaxHp;
             isDead = false;
         }
 
-        protected virtual void TakeDamage(int amount)
+        private void TakeDamage(int amount)
         {
-            // 데미지를 입었을 때 
-            CurrentHp -= amount;
+            // 데미지를 입었을 때 / 무적 판정 필요
+            if (isHitting || isDead)
+                return;
+
+            sum_Damage += amount;
             
-            // 피격 이벤트?
-            if (!isDead)
-            {
-                OnDamaged?.Invoke(amount);
-            }
+            StartCoroutine(InvincibilityTime(amount));
+        }
+        // 무적 시간을 주기 위한 코루틴
+        IEnumerator InvincibilityTime(int amount)
+        {
+            isHitting = true;
+            CurrentHp -= amount;
+
+            OnDamaged?.Invoke();
+
+            yield return new WaitForSeconds(invincibilityTime);
+
+            isHitting = false;
         }
 
-        protected virtual void TakeHeal(int amount)
+        private void TakeHeal(int amount)
         {
             // 힐을 했을 때
             CurrentHp += amount;
-            
+            sum_Healing += amount;
+
             // 힐 이펙트?
+            if (!isDead)
+                OnHealed?.Invoke();
+        }
+
+        // ************* For Debug ************
+        public void Damage_Btn()
+        {
+            TakeDamage(10);
+        }
+
+        public void Heal_Btn()
+        {
+            TakeHeal(10);
         }
     }
 }
